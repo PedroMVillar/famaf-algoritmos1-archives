07f5ea3a84d6a096d9c389cdbffe3ac53d05dd54
PedroMVillar <pedro.villar@mi.unc.edu.ar>
2023-09-14 22:31:21 -0300

    update

--- Proyectos/Proyecto 2/Proyecto2.hs
+++ Proyectos/Proyecto 2/Proyecto2.hs
@@ -1,5 +1,5 @@
 -- ◆═════════════════════════════════════════════●★●═════════════════════════════════════════════◆
-{-En este proyecto nos introducimos en la definici ́on de nuestros propios tipos de datos. La 
+{-En este proyecto nos introducimos en la definición de nuestros propios tipos de datos. La 
 importancia de poder definir nuevos tipos de datos reside en la facilidad con la que podemos modelar 
 problemas y resolverlos usando las mismas herramientas que para los tipos pre-existentes.
 El objetivo de este proyecto es aprender a declarar nuevos tipos de datos en Haskell y definir
