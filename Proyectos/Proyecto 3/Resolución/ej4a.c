36ac3b63f481913e36e04749ac8dc94beac3fa50
PedroMVillar <pedro.villar@mi.unc.edu.ar>
2023-10-09 11:53:53 -0300

    Ejercicio 4 subido

---
+++ Proyectos/Proyecto 3/Resolución/ej4a.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void unoE(void){
+  printf("Punto 1.e\n");
+  int x, y;
+  
+  printf("Ingrese el valor de x: ");
+  scanf("%d", &x);
+  printf("Ingrese el valor de y: ");
+  scanf("%d", &y);
+
+  printf("σ0 (x->%d, y->%d)\n", x, y);
+
+  if (x >= y){
+    printf("σ1 (x->%d, y->%d)\n", x, y);
+    x = 0;
+    printf("σ2 (x->%d, y->%d)\n", x, y);
+  }
+
+  if (x<= y){
+    printf("σ1 (x->%d, y->%d)\n", x, y);
+    x = 2;
+    printf("σ2 (x->%d, y->%d)\n", x, y);
+  }
+
+  printf("σ3 (x->%d, y->%d)\n", x, y);
+}
+
+void unoF(void){
+  printf("Punto 1.f\n");
+  int x, y;
+
+  printf("Ingrese el valor de x: ");
+  scanf("%d", &x);
+  printf("Ingrese el valor de y: ");
+  scanf("%d", &y);
+
+  printf("σ0 (x->%d, y->%d)\n", x, y);
+  if (x >= y){
+    printf("σ1 (x->%d, y->%d)\n", x, y);
+    x = 0;
+    printf("σ2 (x->%d, y->%d)\n", x, y);
+  }  
+  if (x<= y){
+    printf("σ1 (x->%d, y->%d)\n", x, y);
+    x = 2;
+    printf("σ2 (x->%d, y->%d)\n", x, y);
+  }
+
+  printf("σ3 (x->%d, y->%d)\n", x, y);
+}
+
+int main(void) {
+  unoE();
+  unoF();
+  return 0;
+}
