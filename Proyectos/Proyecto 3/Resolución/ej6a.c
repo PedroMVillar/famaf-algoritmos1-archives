ace41f06d7c39949e6f06e4b3255b67410475bb1
PedroMVillar <pedro.villar@mi.unc.edu.ar>
2023-10-09 11:54:25 -0300

    Ejercicio 6a subido

---
+++ Proyectos/Proyecto 3/Resolución/ej6a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int pedir_entero(char name){
+  int x;
+  printf("Ingrese el valor para la variable %c: ", name);
+  scanf("%d", &x);
+  return x;
+}
+
+void imprimir_entero(char name, int x) {
+  printf("La variable %c es %d\n", name, x);
+}
+
+int main(void) {
+  int y = pedir_entero('n');
+  imprimir_entero('n', y);
+  return 0;
+}
